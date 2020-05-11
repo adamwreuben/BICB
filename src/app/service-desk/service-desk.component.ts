1b093c1d643c80993dba17111ceb956df59992ec
Diggerati <innocentmasuki99@gmail.com>
2020-05-11 13:35:18 +0300

    adding lists

--- src/app/service-desk/service-desk.component.ts
+++ src/app/service-desk/service-desk.component.ts
@@ -1,12 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-
+export interface Section {
+  name: string;
+  updated: Date;
+}
 @Component({
   selector: 'app-service-desk',
   templateUrl: './service-desk.component.html',
   styleUrls: ['./service-desk.component.css']
 })
 export class ServiceDeskComponent implements OnInit {
-
+  folders: Section[] = [
+    {
+      name: 'Photos',
+      updated: new Date('1/1/16'),
+    },
+    {
+      name: 'Recipes',
+      updated: new Date('1/17/16'),
+    },
+    {
+      name: 'Work',
+      updated: new Date('1/28/16'),
+    }
+  ];
+  notes: Section[] = [
+    {
+      name: 'Vacation Itinerary',
+      updated: new Date('2/20/16'),
+    },
+    {
+      name: 'Kitchen Remodel',
+      updated: new Date('1/18/16'),
+    }
+  ];
   constructor() { }
 
   ngOnInit(): void {
