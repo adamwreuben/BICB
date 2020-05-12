67a8035150b2910979518714d1afc4a5bec80a4d
Innocent Masuki <innocentmasuki99@gmail.com>
2020-05-13 02:34:06 +0300

    added csv reader module attached it in management component

--- src/app/csv-reader/csv-reader.component.ts
+++ src/app/csv-reader/csv-reader.component.ts
@@ -7,11 +7,9 @@ import { CSVRecord } from './CSVModel';
   styleUrls: ['./csv-reader.component.css']
 })
 export class CsvReaderComponent implements OnInit {
-  title = 'Angular7-readCSV';
   public records: any[] = [];
   @ViewChild('csvReader') csvReader: any;
   uploadListener($event: any): void {
-    let text = [];
     let files = $event.srcElement.files;
     if (this.isValidCSVFile(files[0])) {
       let input = $event.target;
@@ -37,12 +35,21 @@ export class CsvReaderComponent implements OnInit {
       let curruntRecord = (<string>csvRecordsArray[i]).split(',');
       if (curruntRecord.length == headerLength) {
         let csvRecord: CSVRecord = new CSVRecord();
-        csvRecord.id = curruntRecord[0].trim();
-        csvRecord.firstName = curruntRecord[1].trim();
-        csvRecord.lastName = curruntRecord[2].trim();
-        csvRecord.age = curruntRecord[3].trim();
-        csvRecord.position = curruntRecord[4].trim();
-        csvRecord.mobile = curruntRecord[5].trim();
+        csvRecord.customerNumber = curruntRecord[0].trim();
+        csvRecord.clientNature = curruntRecord[1].trim();
+        csvRecord.customerName = curruntRecord[2].trim();
+        csvRecord.gender = curruntRecord[3].trim();
+        csvRecord.birthDate = curruntRecord[4].trim();
+        csvRecord.maritalStatus = curruntRecord[5].trim();
+        csvRecord.idType = curruntRecord[6].trim();
+        csvRecord.idNumber = curruntRecord[7].trim();
+        csvRecord.spauseName = curruntRecord[8].trim();
+        csvRecord.residentialAdress = curruntRecord[9].trim();
+        csvRecord.clientPhoneNumber = curruntRecord[10].trim();
+        csvRecord.spausePhoneNumber = curruntRecord[11].trim();
+        csvRecord.businessType = curruntRecord[12].trim();
+        csvRecord.businessLocation = curruntRecord[13].trim();
+        csvRecord.loanStatus = curruntRecord[14].trim();
         csvArr.push(csvRecord);
       }
     }
